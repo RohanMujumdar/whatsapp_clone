e47d69fa3dbe60a9a33473060ad92b4f3a943158
Rohan Mujumdar <rohanmujumdar2001@gmail.com>
2024-08-16 18:49:50 +0530

    New Changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { Fingerprint } from 'lucide-react';
+import { LogIn } from "lucide-react";
 function Login()
 {
     return ( 
@@ -12,9 +13,13 @@ function Login()
         </div> 
         <div className="flex justify-center items-center bg-gray-100 h-[calc(100vh-220px)] relative">
             <div className="bg-white w-[50%] h-[80%] shadow-2xl flex flex-col justify-center items-center gap-4 absolute -top-[93px]">
-                <div>Sign In</div>
-                <div>Sign In with Google account to get started</div>
-                <button>Sign In with Google</button>
+                <Fingerprint className="h-20 w-20 text-[#10c8a0]" strokeWidth={1.5}/>
+                <div className="font-bold text-2xl leading-8">Sign In</div>
+                <div className="text-gray-500 font-bold">Sign In with Google account to get started</div>
+                <button className="flex gap-2 item-center bg-[#11d1a7] p-3 mt-7 text-white rounded-lg">
+                    Sign In with Google <LogIn />
+                </button>
+
             </div>
         </div>
     </>
